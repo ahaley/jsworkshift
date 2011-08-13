6651bba6b9e346386784a5775233bf6a4e948092
Antonio Haley <antonio.haley@gmail.com>
2011-08-13 11:34:38 -0400

    added server side start/stop support, unified tabs

--- js/test/workshift.js
+++ js/test/workshift.js
@@ -1,105 +1,105 @@
 $(document).ready(function () {
 
-    test("existence of Workshift test", function () {
-        ok(Workshift, "Workshift exists");
-    });
+	test("existence of Workshift test", function () {
+		ok(Workshift, "Workshift exists");
+	});
 
-    test("instantiation of Workshift", function () {
-        var myWorkshift = new Workshift();
-        ok(myWorkshift instanceof Workshift, "instance check");
-    });
+	test("instantiation of Workshift", function () {
+		var myWorkshift = new Workshift();
+		ok(myWorkshift instanceof Workshift, "instance check");
+	});
 
-    test("isSameDay test", function () {
-        var d1 = new Date(2010, 1, 1),
-            d2 = new Date(2010, 1, 1),
-            d3 = new Date(2010, 1, 2);
-        ok(d1.isSameDay, "isSameDay method exists");
-        ok(d1.isSameDay(d1) == true, "identity");
-        ok(d1.isSameDay(d2) == true, "same day");
-        ok(d1.isSameDay(d3) == false, "different day");
-    });
+	test("isSameDay test", function () {
+		var d1 = new Date(2010, 1, 1),
+			d2 = new Date(2010, 1, 1),
+			d3 = new Date(2010, 1, 2);
+		ok(d1.isSameDay, "isSameDay method exists");
+		ok(d1.isSameDay(d1) == true, "identity");
+		ok(d1.isSameDay(d2) == true, "same day");
+		ok(d1.isSameDay(d3) == false, "different day");
+	});
 
-    test("Date can parse", function () {
-        var s = "2011-01-09T13:01-02:00";
-        var d = new Date(s);
-        ok(d instanceof Date);
-    });
+	test("Date can parse", function () {
+		var s = "2011-01-09T13:01-02:00";
+		var d = new Date(s);
+		ok(d instanceof Date);
+	});
 
-    test("createWsRow", function () {
-        var myWs = new Workshift();
-        var d1 = new Date(2010, 1, 1, 1, 30);
-        var d2 = new Date(2010, 1, 1, 2, 30);
-        var wsRow = myWs.createWsRow(d1, d2);
-        var children = wsRow.children();
-        equals(children.length, 2, "created two rows");
-        equals($(children[0]).html(), '1:30:00:AM', 'start date for created ws_row');
-        equals($(children[1]).html(), '2:30:00:AM', 'stop date for created ws_row');
-    });
+	test("createWsRow", function () {
+		var myWs = new Workshift();
+		var d1 = new Date(2010, 1, 1, 1, 30);
+		var d2 = new Date(2010, 1, 1, 2, 30);
+		var wsRow = myWs.createWsRow(d1, d2);
+		var children = wsRow.children();
+		equals(children.length, 2, "created two rows");
+		equals($(children[0]).html(), '1:30:00:AM', 'start date for created ws_row');
+		equals($(children[1]).html(), '2:30:00:AM', 'stop date for created ws_row');
+	});
 
-    test("createWsDay", function () {
-        var myWs = new Workshift();
-        var d = new Date(2010, 0, 15);
-        var createdWsDay = myWs.createWsDay(d);
-        ok(createdWsDay.hasClass('ws_day'), 'does createdWsDay have class ws_day');
+	test("createWsDay", function () {
+		var myWs = new Workshift();
+		var d = new Date(2010, 0, 15);
+		var createdWsDay = myWs.createWsDay(d);
+		ok(createdWsDay.hasClass('ws_day'), 'does createdWsDay have class ws_day');
 
-        var createdWsDate = createdWsDay.children();
+		var createdWsDate = createdWsDay.children();
 
-        equals(createdWsDate.length, 1 , 'created ws_day should only have 1 child');
-        ok(createdWsDate.hasClass('ws_date'));
-        equals(createdWsDate.html(), '1-15-2010', 'ws_date value');
-    });
+		equals(createdWsDate.length, 1 , 'created ws_day should only have 1 child');
+		ok(createdWsDate.hasClass('ws_date'));
+		equals(createdWsDate.html(), '1-15-2010', 'ws_date value');
+	});
 
-    test("insertWsRow with existing date", function () {
-        var target = $('<div id="test"><div class="ws_day"><div class="ws_date">1-15-2010</div></div></div>');
-        var myWs = new Workshift({'target': target});
-        var d1 = new Date(2010, 0, 15, 1, 30);
-        var d2 = new Date(2010, 0, 15, 2, 30);
-        
-        myWs.insertWsRow(d1, d2);
+	test("insertWsRow with existing date", function () {
+		var target = $('<div id="test"><div class="ws_day"><div class="ws_date">1-15-2010</div></div></div>');
+		var myWs = new Workshift({'target': target});
+		var d1 = new Date(2010, 0, 15, 1, 30);
+		var d2 = new Date(2010, 0, 15, 2, 30);
+		
+		myWs.insertWsRow(d1, d2);
 
-        var wsDay = target.children();
-        ok(wsDay.hasClass('ws_day'), 'ws_day has class');
-        equals(wsDay.length, 1, 'no new ws_day was created');
+		var wsDay = target.children();
+		ok(wsDay.hasClass('ws_day'), 'ws_day has class');
+		equals(wsDay.length, 1, 'no new ws_day was created');
 
-        var wsRow = $('.ws_row', wsDay);
-        equals(wsRow.length, 1, 'only one row inserted');
-        equals(wsRow.children().length, 2, 'start and stop children in row');
-    });
+		var wsRow = $('.ws_row', wsDay);
+		equals(wsRow.length, 1, 'only one row inserted');
+		equals(wsRow.children().length, 2, 'start and stop children in row');
+	});
 
-    test("insertWsRow grows up with later date", function () {
-        var target = $('<div id="test"><div class="ws_day"><div class="ws_date">1-15-2010</div><div class="ws_row"><div class="ws_start">1:30:00AM</div><div class="ws_stop">2:30:00AM</div></div></div>');
-        var myWs = new Workshift({'target': target});
-        var d1 = new Date(2010, 5, 10, 1, 30);
-        var d2 = new Date(2010, 5, 10, 2, 30);
-        
-        myWs.insertWsRow(d1, d2);
+	test("insertWsRow grows up with later date", function () {
+		var target = $('<div id="test"><div class="ws_day"><div class="ws_date">1-15-2010</div><div class="ws_row"><div class="ws_start">1:30:00AM</div><div class="ws_stop">2:30:00AM</div></div></div>');
+		var myWs = new Workshift({'target': target});
+		var d1 = new Date(2010, 5, 10, 1, 30);
+		var d2 = new Date(2010, 5, 10, 2, 30);
+		
+		myWs.insertWsRow(d1, d2);
 
-        var days = $('.ws_day', target);
-        equals(days.length, 2, 'created a second day');
-        equals($('.ws_date:first', days).html(), '6-10-2010', 'later date should be first');
-    });
+		var days = $('.ws_day', target);
+		equals(days.length, 2, 'created a second day');
+		equals($('.ws_date:first', days).html(), '6-10-2010', 'later date should be first');
+	});
 
-    test("insertWsRow with new date", function () {
-        var target = $('<div id="test"></div>');
-        var myWs = new Workshift({'target': target});
-        var d1 = new Date(2010, 5, 10, 1, 30);
-        var d2 = new Date(2010, 5, 10, 2, 30);
-         
-        myWs.insertWsRow(d1, d2);
+	test("insertWsRow with new date", function () {
+		var target = $('<div id="test"></div>');
+		var myWs = new Workshift({'target': target});
+		var d1 = new Date(2010, 5, 10, 1, 30);
+		var d2 = new Date(2010, 5, 10, 2, 30);
+		 
+		myWs.insertWsRow(d1, d2);
 
-        var wsDay = target.children();
-        equals(wsDay.length, 1, 'one ws_day created');
-        ok(wsDay.hasClass('ws_day'), 'target has ws_day class after insertWsRow');
-        
-        var wsDate = $('.ws_date', wsDay);
-        equals(wsDate.length, 1, 'one ws_date created');
-        equals(wsDate.html(), '6-10-2010', 'ws_date contains date');
+		var wsDay = target.children();
+		equals(wsDay.length, 1, 'one ws_day created');
+		ok(wsDay.hasClass('ws_day'), 'target has ws_day class after insertWsRow');
+		
+		var wsDate = $('.ws_date', wsDay);
+		equals(wsDate.length, 1, 'one ws_date created');
+		equals(wsDate.html(), '6-10-2010', 'ws_date contains date');
 
-        var wsRows = $('.ws_row', wsDay);
-        equals(wsRows.length, 1, 'one ws_row was created');
-        equals(wsRows.children().length, 2, 'two divs under ws_row(ws_start, ws_stop) were created');
-        equals($('.ws_start', wsRows).html(), '1:30:00:AM', 'ws_start time correct');
-        equals($('.ws_stop', wsRows).html(), '2:30:00:AM', 'ws_stop time correct');
-    });
+		var wsRows = $('.ws_row', wsDay);
+		equals(wsRows.length, 1, 'one ws_row was created');
+		equals(wsRows.children().length, 2, 'two divs under ws_row(ws_start, ws_stop) were created');
+		equals($('.ws_start', wsRows).html(), '1:30:00:AM', 'ws_start time correct');
+		equals($('.ws_stop', wsRows).html(), '2:30:00:AM', 'ws_stop time correct');
+	});
 
 });
--- js/workshift.js
+++ js/workshift.js
@@ -1,153 +1,164 @@
 Date.prototype.isSameDay = function (d) {
-    if (!(d instanceof Date)) {
-        return false;
-    }
-    return (this.getDate() == d.getDate() &&
-        this.getMonth() == d.getMonth() &&
-        this.getYear() == d.getYear());
+	if (!(d instanceof Date)) {
+		return false;
+	}
+	return (this.getDate() == d.getDate() &&
+		this.getMonth() == d.getMonth() &&
+		this.getYear() == d.getYear());
 }
 
 Date.prototype.toFormatDate = function () {
-    var date = this.getDate();
-    var month = this.getMonth() + 1;
-    var year = this.getFullYear();
-    return month + '-' + date + '-' + year;
+	var date = this.getDate();
+	var month = this.getMonth() + 1;
+	var year = this.getFullYear();
+	return month + '-' + date + '-' + year;
 }
 
 Date.prototype.toFormatTime = function () {
-    var pad2 = function (s) {
-        if (s < 10) return '0' + s;
-        return s;
-    }
-    var hour = this.getHours();
-    var meridiem = hour > 11 ? "PM" : "AM";
-    hour = hour % 12;
-    if (hour == 0) hour = 12;
-    var minute = pad2(this.getMinutes())
-    var seconds = pad2(this.getSeconds());
-    return hour + ":" + minute + ":" + seconds + ":" + meridiem;
+	var pad2 = function (s) {
+		if (s < 10) return '0' + s;
+		return s;
+	}
+	var hour = this.getHours();
+	var meridiem = hour > 11 ? "PM" : "AM";
+	hour = hour % 12;
+	if (hour == 0) hour = 12;
+	var minute = pad2(this.getMinutes())
+	var seconds = pad2(this.getSeconds());
+	return hour + ":" + minute + ":" + seconds + ":" + meridiem;
 }
-    
+	
 var Workshift = (function () {
 
-    var Workshift = function (options) {
-        this.init(options);
-    };
+	var Workshift = function (options) {
+		this.init(options);
+	};
 
-    Workshift.prototype = function () {
-        var that = null;
+	Workshift.prototype = function () {
+		var that = null;
 
-        var init = function (options) {
-            that = this;
-            if (options) {
-                if (options.target)
-                    this.target = options.target;
-                if (options.timeScaffold) {
-                    this.timeScaffold = options.timeScaffold;
-                }
-                if (options.toggle) {
-                    this.toggle = options.toggle;
-                    this.toggle.click(function () { startTimer() });
-                }
-            }
-        }
+		var init = function (options) {
+			that = this;
+			if (options) {
+				if (options.target)
+					this.target = options.target;
+				if (options.timeScaffold) {
+					this.timeScaffold = options.timeScaffold;
+				}
+				if (options.toggle) {
+					this.toggle = options.toggle;
+					this.toggle.click(function () { startTimer() });
+				}
+				if (options.startURI) {
+					this.startURI = options.startURI;
+				}
+				if (options.stopURI) {
+					this.stopURI = options.stopURI;
+				}
+			}
+		}
 
-        var createWsRow = function (startDate, stopDate) {
-            return $('<div class="ws_row"></div>')
-                .append($('<div class="ws_start"></div>').append(startDate.toFormatTime()))
-                .append($('<div class="ws_stop"></div>').append(stopDate.toFormatTime()));
-        }
+		var createWsRow = function (startDate, stopDate) {
+			return $('<div class="ws_row"></div>')
+				.append($('<div class="ws_start"></div>').append(startDate.toFormatTime()))
+				.append($('<div class="ws_stop"></div>').append(stopDate.toFormatTime()));
+		}
 
-        var createWsDay = function (currentDate) {
-            return $('<div class="ws_day"></div>')
-                .append($('<div class="ws_date"></div>')
-                    .append(currentDate.toFormatDate()));
-        }
+		var createWsDay = function (currentDate) {
+			return $('<div class="ws_day"></div>')
+				.append($('<div class="ws_date"></div>')
+					.append(currentDate.toFormatDate()));
+		}
+	
+		var insertWsRow = function (startDate, stopDate) {
+			var wsRow = createWsRow(startDate, stopDate);
+			var wsDate = $('.ws_date:first', that.target);
+			var currentDate = new Date(wsDate.html());
+			if (currentDate.isSameDay(startDate)) {
+				wsDate.after(wsRow);
+			}
+			else {
+				var wsDay = createWsDay(startDate);
+				wsDay.append(wsRow);
+				that.target.prepend(wsDay);
+			}
+			return wsRow;
+		}
 
-        var insertWsRow = function (startDate, stopDate) {
-            var wsRow = createWsRow(startDate, stopDate);
-            var wsDate = $('.ws_date:first', that.target);
-            var currentDate = new Date(wsDate.html());
-            if (currentDate.isSameDay(startDate)) {
-                wsDate.after(wsRow);
-            }
-            else {
-                var wsDay = createWsDay(startDate);
-                wsDay.append(wsRow);
-                that.target.prepend(wsDay);
-            }
-            return wsRow;
-        }
+		var _startTimer = function (startTime) {
+			var msStartServer = startTime.getTime();
+			var msStartLocal = (new Date()).getTime();
 
-        var startTimer = function () {
-            var now = that.timeScaffold ? that.timeScaffold() : new Date();
-            that.toggle.val('Stop');
-            that.toggle.unbind('click');
-            that.toggle.click(function () { stopTimer() });
-            
-            // TODO: AJAX call to create server-side Workshift, ie. retrieve Start Date. s/now/startServer
+			that.wsStop = $('.ws_stop', insertWsRow(startTime, startTime));
+			
+			var updateClock = function () {
+				var msStopLocal = (new Date()).getTime();
+				var stopServerPrime = new Date(msStopLocal - msStartLocal + msStartServer);
+				that.wsStop.html(stopServerPrime.toFormatTime());
+				that.timerID = setTimeout(updateClock, 1000);
+			}
+			that.timerID = setTimeout(updateClock, 1000);
+		}
 
-            var createdRow = createWsRow(now, now);
-            var msStartServer = now.getTime();
-            var msStartLocal = (new Date()).getTime();
+		var startTimer = function () {
+			that.toggle.val('Stop');
+			that.toggle.unbind('click');
+			that.toggle.click(function () { stopTimer() });
+			
+			if (that.startURI) {
+				$.post(that.startURI, function (data) {
+					_startTimer(new Date(data.start));
+				});
+			}
+			else {
+				var now = that.timeScaffold ? that.timeScaffold() : new Date();
+				_startTimer(now);
+			}
+		}
 
-            var createdRow = insertWsRow(now, now);
+		var stopTimer = function () {
+			var now = that.timeScaffold ? that.timeScaffold() : new Date();
+			clearTimeout(that.timerID);
+			that.wsStop.html(now.toFormatTime());
+			that.toggle.val('Start');
+			that.toggle.unbind('click');
+			that.toggle.click(function () { startTimer() });
+		}
 
-            that.wsStop = $('.ws_stop', createdRow);
-            
-            var updateClock = function () {
-                var msStopLocal = (new Date()).getTime();
-                var stopServerPrime = new Date(msStopLocal - msStartLocal + msStartServer);
-                that.wsStop.html(stopServerPrime.toFormatTime());
-                that.timerID = setTimeout(updateClock, 1000);
-            }
-            that.timerID = setTimeout(updateClock, 1000);
+		var renderWorkshift = function (workshifts) {
 
-        }
+			var currentDate = null,
+				wsDay = null,
+				that = this;
+				
+			$.each(workshifts, function(index, workshift) {
+				
+				var startDate = new Date(workshift.start);
+				var stopDate = new Date(workshift.stop);
 
-        var stopTimer = function () {
-            var now = that.timeScaffold ? that.timeScaffold() : new Date();
-            clearTimeout(that.timerID);
-            that.wsStop.html(now.toFormatTime());
-            that.toggle.val('Start');
-            that.toggle.unbind('click');
-            that.toggle.click(function () { startTimer() });
-        }
+				if (!startDate.isSameDay(currentDate)) {
+					currentDate = startDate;
+					that.target.append(wsDay);
+					wsDay = createWsDay(currentDate);
+				}
+				wsDay.append(createWsRow(startDate, stopDate));
+			
+			});
 
-        var renderWorkshift = function (workshifts) {
+			this.target.append(wsDay);
 
-            var currentDate = null,
-                wsDay = null,
-                that = this;
-                
-            $.each(workshifts, function(index, workshift) {
-                
-                var startDate = new Date(workshift.start);
-                var stopDate = new Date(workshift.stop);
+		}
 
-                if (!startDate.isSameDay(currentDate)) {
-                    currentDate = startDate;
-                    that.target.append(wsDay);
-                    wsDay = createWsDay(currentDate);
-                }
-                wsDay.append(createWsRow(startDate, stopDate));
-            
-            });
+		return {
+			init: init,
+			createWsRow: createWsRow,
+			createWsDay: createWsDay,
+			insertWsRow: insertWsRow,
+			startTimer: startTimer,
+			renderWorkshift: renderWorkshift
+		};
+	} ();
 
-            this.target.append(wsDay);
-
-        }
-
-        return {
-            init: init,
-            createWsRow: createWsRow,
-            createWsDay: createWsDay,
-            insertWsRow: insertWsRow,
-            startTimer: startTimer,
-            renderWorkshift: renderWorkshift
-        };
-    } ();
-
-    return Workshift;
+	return Workshift;
 })();
 
