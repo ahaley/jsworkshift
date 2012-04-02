115c56327e242f91e93f2abc56d99b9bcec383cd
Antonio Haley <antonio.haley@gmail.com>
2012-04-02 08:59:53 -0400

    added WorkshiftDiff class to handle diff calculations

---
+++ js/test/workshift_diff.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    test("existence", function () {
+        ok(WorkshiftDiff, "WorkshiftDiff exists");
+    });
+
+    test("WorkshiftDiff can calculate total hours", function () {
+        var diffs = ['4h 27m', '3h 54m', '4h 54m', '3h 58m'];
+        var result = WorkshiftDiff.sum(diffs);
+        equals(result, '17h 13m');
+    });
+
+    test("WorkshiftDiff can calculate total hours from wrapped set", function () {
+        var diffs = $('.ws_diff');
+        var result = WorkshiftDiff.sumWrappedSet(diffs);
+        equals(result, '17h 13m');
+    });
+
+    test("WorkshiftDiff can calculate total hours from selctor", function() {
+        var result = WorkshiftDiff.sumSelector('.ws_diff');
+        equals(result, '17h 13m');
+    });
+});
---
+++ js/workshift_diff.js
@@ -0,0 +1,33 @@
+﻿WorkshiftDiff = {};
+
+WorkshiftDiff.sum = function (diffs) {
+    var sum_hour = 0;
+    var sum_minute = 0;
+    var regex = /(\d+)h (\d+)m/
+
+    for (var i = 0; i < diffs.length; i++) {
+        var diff = diffs[i];
+        var matches = regex.exec(diff);
+        if (matches != null && matches.length && matches.length == 3) {
+            sum_hour += parseInt(matches[1]) || 0;
+            sum_minute += parseInt(matches[2]) || 0;
+        }
+    }
+
+    var hour = sum_hour + Math.floor(sum_minute / 60);
+    var minute = sum_minute % 60;
+
+    return hour + 'h ' + minute + 'm';
+}
+
+WorkshiftDiff.sumWrappedSet = function (diffs) {
+    var value_array = [];
+    $.each(diffs, function (index, value) {
+        value_array.push($(value).html());
+    });
+    return WorkshiftDiff.sum(value_array);
+}
+
+WorkshiftDiff.sumSelector = function (selector) {
+    return WorkshiftDiff.sumWrappedSet($(selector));
+}
